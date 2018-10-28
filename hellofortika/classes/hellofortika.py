b0b18ad25385af651a079524af08e22f4b122ddd
George <geosakel@uom.edu.gr>
2018-10-28 07:39:26 +0200

    minor changes

--- hellofortika/classes/hellofortika.py
+++ hellofortika/classes/hellofortika.py
@@ -18,5 +18,12 @@ class HelloFortika:
         Put a short description here
         @return: 0
         """
-        print(self._fortika)
-        return 0
+        try:
+            print(self._fortika)
+            return 0
+        except OSError as e:
+            print(e.args)
+            return -1
+        else:
+            print("General Exception Handling")
+            return 1
